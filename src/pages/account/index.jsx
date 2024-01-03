d2dbad72fa59bec5205375869dde05d6465e9a32
wumusenlin000 <wumusenlin000@163.com>
2024-01-03 16:35:30 +0800

    feat: add ThemeProvider and react-router-prompt

--- src/pages/account/index.jsx
+++ src/pages/account/index.jsx
@@ -1,7 +1,7 @@
 import { useNavigate } from 'react-router-dom'
-import { Button } from 'antd'
+import { Button, Modal } from 'antd'
 import './account.styl'
-
+import ReactRouterPrompt from "react-router-prompt";
 function Account() {
 
     // 创建路由钩子
@@ -11,8 +11,29 @@ function Account() {
         <div className="P-account">
             <h1>Account Page</h1>
             <div className="ipt-con">
-                <Button type="primary" onClick={()=>{navigate('/login')}}>返回登录</Button>
+                <Button type="primary" onClick={() => { navigate('/login') }}>返回登录</Button>
             </div>
+            <ReactRouterPrompt
+                when={true}
+            // beforeConfirm={async () => {
+            //     await delayPromise();
+            //     await fetch("https://api.zippopotam.us/in/400072")
+            //         .then((response) => response.text())
+            //         .then((result) => alert("called beforeConfirm " + result))
+            //         .catch((error) => console.log("error", error));
+            // }}
+            // beforeCancel={() => delayPromise()}
+            >
+                {({ isActive, onConfirm, onCancel }) =>
+                    isActive &&
+                    <>
+                        <Modal visible={isActive} onOk={onConfirm} onCancel={onCancel}>
+                            确认退出吗？
+                        </Modal>
+                    </>
+
+                }
+            </ReactRouterPrompt>
         </div>
     )
 }
