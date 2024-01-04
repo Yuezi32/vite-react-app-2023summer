6deec63ce3bb6f715a1502dee852c0dfc4ce782a
wumusenlin000 <wumusenlin000@163.com>
2024-01-04 17:47:56 +0800

    feat: 完善login

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react'
 import { apiReqs } from '@/api'
 import { useNavigate } from 'react-router-dom'
-import { Button, Input, Card, Col, Row } from 'antd'
-import imgLogo from './logo.png'
+import { Button, Input, Card, Col, Row, Carousel, Divider } from 'antd'
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import tuzi from './tuzi.png'
+import wumu from './wumu.png'
 import ThemeProvider from '../../components/ConfigProvider/ThemeProvider'
 import LoginFooter from './components/LoginFooter'
 import './login.less'
@@ -33,40 +35,52 @@ function Login() {
     return (
         <ThemeProvider>
             <div className="P-login">
-
                 <div className='login-box'>
-                    <Card bordered={false}>
+                    <Card bordered={false} bodyStyle={{ padding: 0, height: '100%' }}>
                         <Row>
-                            <Col span={12}>
-
-                                <div className="logo">
-                                    <img src={imgLogo} alt="" />
-                                </div>
-
+                            <Col span={12} style={{ background: '#ccc' }}>
+                                <Carousel rootClassName='login-carousel' autoplay>
+                                    <div className="login-carousel">
+                                        <img className="login-carousel-img" src={wumu} alt="" />
+                                    </div>
+                                    <div className="login-carousel">
+                                        <img className="login-carousel-img" src={tuzi} alt="" />
+                                    </div>
+                                </Carousel>
                             </Col>
                             <Col span={12}>
-                                <div className="ipt-con">
-                                    <Input
-                                        placeholder="账号"
-                                        value={account}
-                                        onChange={(e) => {
-                                            setAccount(e.target.value)
-                                        }}
-                                    />
-                                </div>
-                                <div className="ipt-con">
-                                    <Input.Password
-                                        placeholder="密码"
-                                        value={password}
-                                        onChange={(e) => {
-                                            setPassword(e.target.value)
-                                        }}
-                                    />
-                                </div>
-                                <div className="ipt-con">
-                                    <Button type="primary" block={true} onClick={login}>
-                                        登录
-                                    </Button>
+                                <div className='login-form-box'>
+                                    <h1 style={{ fontSize: '24px' }}>wumusenlin</h1>
+                                    <Divider />
+                                    <h2>账密登录</h2>
+                                    <br />
+                                    <div className="ipt-con">
+                                        <Input
+                                            size="large"
+                                            prefix={<UserOutlined />}
+                                            placeholder="账号"
+                                            value={account}
+                                            onChange={(e) => {
+                                                setAccount(e.target.value)
+                                            }}
+                                        />
+                                    </div>
+                                    <div className="ipt-con">
+                                        <Input.Password
+                                            prefix={<LockOutlined />}
+                                            size="large"
+                                            placeholder="密码"
+                                            value={password}
+                                            onChange={(e) => {
+                                                setPassword(e.target.value)
+                                            }}
+                                        />
+                                    </div>
+                                    <div className="ipt-btn">
+                                        <Button size="large" type="primary" block={true} onClick={login}>
+                                            登录
+                                        </Button>
+                                    </div>
                                 </div>
                             </Col>
                         </Row>
